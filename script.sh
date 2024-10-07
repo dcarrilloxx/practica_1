d71a466c7cf89083831cf9a4e4e59fc838c61374
Carrillo <carrilloxx@Dcarrillo>
2024-10-07 09:03:07 +0200

    dia 2

--- script.sh
+++ script.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+cut -d ',' -f 1-11,13-15 supervivents.csv > temporal.csv && mv temporal.csv supervivents.csv
+awk -F',' '$15 != "True"' supervivents.csv > temporal.csv && mv temporal.csv supervivents.csv
+
+wc -l supervivents.csv
+
+awk -F',' 'BEGIN{OFS=","} {if($8<=1000000) $17 = "Bo"; else if ($8<=10000000) $17="Excel·lent"; else $17="Estrella"; print $0}' supervivents.csv > temp.csv && mv temp.csv supervivents.csv
+
+
 cut -d ',' -f 1-11,13-15 supervivents.csv > temporal.csv
-head -n 1 temporal.csv | tr ',' '\n' | nl
-
- 
-
+awk -F',' '$15 != "True"' temporal.csv > temporal2.csv
 
