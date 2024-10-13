c967101d35a1447dceb237d3da1b95af14a61fee
Carrillo <carrilloxx@Dcarrillo>
2024-10-13 21:10:03 +0200

    dia 3

--- script.sh
+++ script.sh
@@ -1,12 +1,33 @@
+
 #!/bin/bash
-cut -d ',' -f 1-11,13-15 supervivents.csv > temporal.csv && mv temporal.csv supervivents.csv
-awk -F',' '$15 != "True"' supervivents.csv > temporal.csv && mv temporal.csv supervivents.csv
-
-wc -l supervivents.csv
-
-awk -F',' 'BEGIN{OFS=","} {if($8<=1000000) $17 = "Bo"; else if ($8<=10000000) $17="Excel·lent"; else $17="Estrella"; print $0}' supervivents.csv > temp.csv && mv temp.csv supervivents.csv
-
-
+awk -F',' '{ if (NF == 16) print $0 }' CAvideos.csv > supervivents.csv
 cut -d ',' -f 1-11,13-15 supervivents.csv > temporal.csv
-awk -F',' '$15 != "True"' temporal.csv > temporal2.csv
+awk -F ',' '{ if ($14 != "True") print $0 }' temporal.csv > temporal2.csv
+
+x=$(wc -l < supervivents.csv)
+y=$(wc -l < temporal2.csv)
+
+diferencia=$((x-y))
+
+echo "$diferencia registres eliminats"
+
+awk -F',' '{
+    if ($8 <= 1000000) ranking="Bo";
+    else if ($8 > 1000000 && $8 <= 10000000) ranking="Excel·lent";
+    else ranking="Estrella";
+    print $0 "," ranking;
+}' temporal2.csv > temporal3.csv
+
+awk -F',' '{
+  if ($8 != 0) {
+    Rlikes = ($9 * 100) / $8;
+    Rdislikes = ($10 * 100) / $8;
+  } else {
+    Rlikes = "No es pot dividir entre 0 o valor invàlid";
+    Rdislikes = "No es pot dividir entre 0 o valor invàlid";
+  }
+  print $0 "," Rlikes "," Rdislikes;
+}' temporal3.csv > sortida.csv
+
+
 
