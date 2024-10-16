e69e71460a435cb7a075557cd29b83469e71f3b9
Carrillo <carrilloxx@Dcarrillo>
2024-10-16 20:42:54 +0200

    practica casi definitiva

--- script.sh
+++ script.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+# Filtrar registres que tenen 16 camps
+awk -F',' '{ if (NF == 16) print $0 }' CAvideos.csv > supervivents.csv
+
+# Eliminar columna 12 i conservar altres columnes
+cut -d ',' -f 1-11,13-15 supervivents.csv > temporal.csv
+
+# Filtrar els registres on la columna 14 no és "True"
+awk -F ',' '{ if ($14 != "True") print $0 }' temporal.csv > temporal2.csv
+
 # Comptar registres eliminats
 x=$(wc -l < supervivents.csv)
 y=$(wc -l < temporal2.csv)
 diferencia=$((x - y))
 echo "$diferencia registres eliminats"
 
-# Afegir columna Ranking_Views
+# Afegir columna "Ranking_Views"
 awk -F',' '{
     if ($8 <= 1000000) ranking="Bo";
     else if ($8 > 1000000 && $8 <= 10000000) ranking="Excel·lent";
@@ -15,7 +24,7 @@ awk -F',' '{
 }' temporal2.csv > temporal3.csv
 
 # Calcular rlikes i rdislikes, si les views no són majors que 0, likes i dislikes són 0
-while IFS=',' read -r id trending title channel category publish tags views likes dislikes comments comment_disabled rating_disabled error
+while IFS=',' read -r id trending title channel category publish tags views likes dislikes comments comment_disabled rating_disabled error ranking
 do
     # Comprovar que views sigui numèric i major que 0
     if [[ "$views" =~ ^[0-9]+$ && "$views" -gt 0 ]]; then
@@ -25,20 +34,26 @@ do
         rlikes=0
         rdislikes=0
     fi
-
-    # Mostrar els resultats ben formatejats
-    echo "VideoID: $id | Title: $title | Views: $views | Likes: $likes | Dislikes: $dislikes | Rlikes: $rlikes% | Rdislikes: $rdislikes%"
+    # Escribir resultados en un archivo CSV de salida
+    echo "VideoID: $id | Title: $title | Views: $views | Likes: $likes | Dislikes: $dislikes | Rlikes: $rlikes% | Rdislikes: $rdislikes% | Ranking: $ranking" >> sortida.csv
 done < temporal3.csv
 
-# Búsqueda de vídeo per títol o ID
-# Búsqueda de vídeo por títol o ID
-if [ -n "$1" ]; then
-    resultat=$(grep -i "$1" temporal3.csv)  # -i para ignorar mayúsculas/minúsculas
-    if [ -z "$resultat" ]; then
-        echo "No s'ha trobat cap vídeo amb aquest títol o identificador."
-    else
-        echo "$resultat" | cut -d',' -f3,6,8,9,10,17,18,19  # Filtrar columnas específicas
-    fi
+# Buscar un vídeo por ID o título
+echo "Introdueix la id o el títol d'un vídeo: "
+read variable
+
+# Comprobar si hay coincidencias en el archivo CSV
+if grep -q "$variable" sortida.csv; then
+    grep "$variable" sortida.csv
+else
+    echo "No s'han trobat coincidencies"
+    echo "Aquí tens la llista de tots els vídeos:"
+    cat sortida.csv
+
 fi
+
+
+
+
 
 
